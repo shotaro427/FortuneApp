50677955bef0ae7b5be07c1f73bf6c401f9b1957
shotaro427 <toshinobu.s111@gmail.com>
2019-08-09 08:50:16 +0900

    重複していた記述の削除+コメントの追加

--- FortunesApp/ViewController.swift
+++ FortunesApp/ViewController.swift
@@ -18,12 +18,11 @@ class ViewController: UIViewController {
     // スライダー
     @IBOutlet weak var sliderValue: UISlider!
     
+    // ラベル系
     // スライダーの値を表示させるラベル
     @IBOutlet weak var sliderValueLabel: UILabel!
-    
     // 占い結果を表示させるtextView
     @IBOutlet weak var resultView: UITextView!
-    
     
     // 占いの表示内容を決定するための変数
     // 生年月日の年
@@ -68,7 +67,6 @@ class ViewController: UIViewController {
             "また、あなたは生命力に溢れ、エネルギッシュに人生を切り開いていく力を持っています。エリートコースを歩む人が多いのが特徴です。",
             "また、あなたはまじめにコツコツと努力を積み重ね、最終的にはすばらしい結果を残し成功する大器晩成型である可能性が高いです。"
         ],
-        
     ]
     
     override func viewDidLoad() {
@@ -77,7 +75,7 @@ class ViewController: UIViewController {
     
     // スライダーが動くごとに呼び出される処理
     @IBAction func moveSlider(_ sender: UISlider) {
-        sliderValueLabel.text = String(Int(ceil(sliderValue.value))) // ラベルに0~100をで表示させる
+        sliderValueLabel.text = String(Int(sliderValue.value)) // ラベルに0~100で表示させる
     }
     
     // 「占う」ボタン
@@ -87,18 +85,16 @@ class ViewController: UIViewController {
         resultView.text = ""
         
         // 以下パラメータの取得
-        // datePickerからdate型の値を取得
-        let date = datePicker.date
         // 年/月/日それぞれを取得
-        year = datePicker.calendar.component(.year, from: date)
-        month = datePicker.calendar.component(.month, from: date)
-        day = datePicker.calendar.component(.day, from: date)
+        year = datePicker.calendar.component(.year, from: datePicker.date)
+        month = datePicker.calendar.component(.month, from: datePicker.date)
+        day = datePicker.calendar.component(.day, from: datePicker.date)
         
         // 血液型を取得(0: A型, 1: B型, 2: O型, 3: AB型)
         bloodIndex = bloodType.selectedSegmentIndex
         
         // スライドバーの値を取得
-        favNum = Int(ceil(sliderValue.value))
+        favNum = Int(sliderValue.value)
         
         // 以下パラメータの加工
         // 各パラメータを4or3で割った余りで、表示する内容を決定する
